c7d3188073f1c8956d232f860cb26bb47beb9d47
Hack Reactor Students <santiago.archila@gmail.com>
2013-11-25 19:56:28 -0800

    implement chatrooms dropdown menu, changed message to pseudoclassical

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,6 +5,26 @@ var Message = function(username, text, roomname){
   this.roomname = roomname;
 };
 
+Message.prototype.send = function(){
+  console.log(JSON.stringify(this));
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(this),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data);
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.log(data);
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
 var printToLog = function (arg) {
   console.log(arg);
 };
@@ -23,56 +43,27 @@ var getMessages = function(cb){
 };
 
 var displayMessage = function (msgs) {
+  $('ul').html('');
   for ( var i = 0 ; i < msgs.length ; i++){
-    // $uname = $('<li></li>');
-    // $uname.html(msgs[i].username);
-    // $msg = $('<li></li>');
-    // $msg.html(msgs[i].text);
-    // $msg.html($uname.text() + ': ' + $msg.text());
-    var unameText = escape(msgs[i].username);
-    if (unameText === undefined){
-      unameText = 'anonymous';
-    }
-    var msgText = escape(msgs[i].text);
-    // console.log(i + msgText.slice(0,2));
-    console.log(msgs[i].text);
-    var safe = true;
-    if (msgText.indexOf("%3Cimg") !== -1 ||
-      unameText.indexOf("%3Cimg") !== -1 ||
-      msgText.indexOf("%3Cscript%3E") !== -1 ||
-      unameText.indexOf("%3Cscript%3E") !==-1) {
-      safe = false;
-    }
-    // if (msgText.indexOf("%3Cscript%3E") !== -1 || unameText.indexOf("%3Cscript%3E") !==-1 ){
-    //   safe = false;
-    // }
-    if (safe === true) {
-      $msg = $('<li>' + i   + unescape(unameText) + ': '+ unescape(msgText) +'</li>');
-      $('ul').prepend($msg);
+    // var rname = msgs[i][0].value;
+    var $uname = $('<li></li>');
+    var $msg = $('<li></li>');
+    $uname.text(msgs[i].username);
+    $msg.text(msgs[i].text);
+    $msg.text($uname.text() + ': ' + $msg.text());
+    $msg.addClass(msgs[i].roomname + ' all');
+    if ($msg.hasClass($('select')[0].value)){
+      $('ul').append($msg);
     }
   }
 };
-
+var username = unescape(window.location.search.slice(10));
 // getMessages(displayMessage);
-var sendMessage = function(message){
-  console.log(JSON.stringify(message));
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log(data);
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      console.log(data);
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-};
+// var msg = new Message(username,'oops','hackreactor');
+// sendMessage(msg);
 
-var msg = new Message('sa','oops','hackreactor');
-sendMessage(msg);
+
+
+
+
+
